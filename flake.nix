301ea3e2bb456d947e8e3f09b0fcc35d0e1f9225
Akshett Rai Jindal <jindalakshett@gmail.com>
2024-06-30 12:28:59 +0530

    flake: initialize flake for development

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+    description = "A basic flake for Golang development";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = {
+        self,
+        nixpkgs,
+        flake-utils,
+    }: flake-utils.lib.eachDefaultSystem(system:
+        let
+            pkgs = import nixpkgs { inherit system; };
+        in {
+
+            devShell = pkgs.mkShell {
+                name = "<name>-dev";
+                nativeBuildInputs = with pkgs; [
+                    go
+                    gopls
+                ];
+                shellHook = ''
+                    export GOPATH="$(git rev-parse --show-toplevel)/.go"
+                '';
+            };
+
+            packages = rec {
+                name = (pkgs.callPackage ./nix/pkgs/name.nix);
+                default = name;
+            };
+
+        }
+    );
+}
